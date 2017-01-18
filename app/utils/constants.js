5a80eea7ea442e39df2fcfa3f18794ea11d97528
tjz <415800467@qq.com>
2017-01-18 18:17:20 +0800

    初始化

--- app/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const ROLE_PERSONAL = 0
-export const ROLE_TEAM = 1
-export const ROLE_COMPANY = 2
-export { default as INTUSTRY_LIST } from './industry'
-export { default as AREA_LIST } from './area'
-export { default as PROFESSION_LIST } from './profession'
